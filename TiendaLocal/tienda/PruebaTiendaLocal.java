8e3ad7238b66f7c0b1c95a9039595e39677b2c1e
amoraru15 <31902593+amoraru15@users.noreply.github.com>
2018-09-24 13:50:17 +0200

    Arreglado Menu

--- TiendaLocal/tienda/PruebaTiendaLocal.java
+++ TiendaLocal/tienda/PruebaTiendaLocal.java
@@ -3,6 +3,8 @@ package tienda;
 import java.io.IOException;
 import java.util.Scanner;
 
+import javafx.scene.web.PromptData;
+
 
 public class PruebaTiendaLocal {
 
@@ -17,19 +19,16 @@ public class PruebaTiendaLocal {
 		// POR IMPLEMENTAR
 
 		int op = 0;
-		do {
 		
-			System.out.println("1. Salir del programa.");
-			System.out.println("2. Listar los autores en el catálogo de la tienda.");
-			System.out.println("3. Buscar un autor con un nombre dado y mostrar sus cómics en la tienda.");
-			System.out.println("4. Comprar un cómic con un código dado, actualizando sus existencias y mostrar sus datos.");
-			System.out.println("5. Revender a la tienda un ejemplar de un cómic de su catálogo con un código dado, actualizando sus existencias y mostrar sus datos.");
-			System.out.println("------------------");
-			System.out.println("Elige una opción: ");
-			op = teclado.nextInt();
-			
-		}while(op < 1 ||  op > 5);
-		teclado.nextLine();
+		System.out.println("1. Salir del programa.");
+		System.out.println("2. Listar los autores en el catálogo de la tienda.");
+		System.out.println("3. Buscar un autor con un nombre dado y mostrar sus cómics en la tienda.");
+		System.out.println("4. Comprar un cómic con un código dado, actualizando sus existencias y mostrar sus datos.");
+		System.out.println("5. Revender a la tienda un ejemplar de un cómic de su catálogo con un código dado, actualizando sus existencias y mostrar sus datos.");
+		System.out.println("------------------");
+		System.out.println("Elige una opción: ");
+		op = teclado.nextInt();
+		
 		return op; // DEVOLVER EL VALOR ADECUADO EN CADA CASO;
 	}
 
@@ -44,35 +43,43 @@ public class PruebaTiendaLocal {
 		GestorConsultas gestor = new GestorConsultas();
 		new GestorConsultas();
 		Scanner teclado = new Scanner(System.in);
-		int select = menu(teclado);
+		int select = 0;
+		while (select != 1) {
+			System.out.println("");
+			System.out.println("Pulsa enter para continuar.");
+			System.in.read();
+			System.out.println("------------------");
+			select = menu(teclado);
 		switch (select) {
-		case 1:
-			System.out.println("CERRANDO EL PROGRAMA DE PRUEBA");
-			gestor.cierraGestor();
-			break;
-		case 2:
-			String[] listaAutores = gestor.listaAutores();
-			for (String autor: listaAutores) System.out.println(autor);
-			break;
-		case 3:
-			System.out.println("Introduce el nombre de un autor: ");
-			String autor = teclado.nextLine();
-			String[] listaComics = gestor.buscaAutor(autor);
-			if (listaComics.length == 0) System.out.println("No se ha encontrado el autor.");
-			else for (String comic: listaComics) System.out.println(comic);
-			break;
-		case 4:
-			System.out.println("Introduce el codigo de un cómic para comprar: ");
-			int codigo = teclado.nextInt();
-			System.out.println(gestor.bajaEjemplar(codigo));
-			break;
-		case 5:
-			System.out.println("Introduce el codigo de un cómic para vender: ");
-			codigo = teclado.nextInt();
-			System.out.println(gestor.altaEjemplar(codigo));
-			break;
-		default:
-			break;
+			case 1:
+				System.out.println("CERRANDO EL PROGRAMA DE PRUEBA");
+				gestor.cierraGestor();
+				break;
+			case 2:
+				String[] listaAutores = gestor.listaAutores();
+				for (String autor: listaAutores) System.out.println(autor);
+				break;
+			case 3:
+				System.out.println("Introduce el nombre de un autor: ");
+				teclado.nextLine();
+				String autor = teclado.nextLine();
+				String[] listaComics = gestor.buscaAutor(autor);
+				if (listaComics.length == 0) System.out.println("No se ha encontrado el autor.");
+				else for (String comic: listaComics) System.out.println(comic);
+				break;
+			case 4:
+				System.out.println("Introduce el codigo de un cómic para comprar: ");
+				int codigo = teclado.nextInt();
+				System.out.println(gestor.bajaEjemplar(codigo));
+				break;
+			case 5:
+				System.out.println("Introduce el codigo de un cómic para vender: ");
+				codigo = teclado.nextInt();
+				System.out.println(gestor.altaEjemplar(codigo));
+				break;
+			default:
+				break;
+			}
 		}
 		teclado.close();
 	} // fin de main
